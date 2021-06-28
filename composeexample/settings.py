99217aa44d7e7c65a80f019e02c39938d8de2a6b
johntziogkas <john.tziog@gmail.com>
2021-06-28 22:46:10 +0300

    added

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'composeexample.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'postgres',
     'USER': 'postgres',
